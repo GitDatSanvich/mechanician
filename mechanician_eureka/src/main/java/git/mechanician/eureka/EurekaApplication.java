718229a8a78ceb020350a635bf183df62448f44a
Gitdatsanvich <tc704321764@outlook.com>
2019-02-25 13:02:36 +0800

    出现了跨域请求 添加Spring cloud

--- mechanician_eureka/src/main/java/git/mechanician/eureka/EurekaApplication.java
+++ mechanician_eureka/src/main/java/git/mechanician/eureka/EurekaApplication.java
@@ -14,6 +14,5 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class);
-
     }
 }
--- mechanician_tools/src/main/java/git/mechanician/tools/ToolsApplication.java
+++ mechanician_tools/src/main/java/git/mechanician/tools/ToolsApplication.java
@@ -1,9 +1,11 @@
 package git.mechanician.tools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 @SpringBootApplication
+@EnableEurekaClient
 public class ToolsApplication {
 
 	public static void main(String[] args) {
