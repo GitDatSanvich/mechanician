28abe4190322f9a8a9e53e7dc57493ab388df72f
Gitdatsanvich <tc704321764@outlook.com>
2019-03-29 18:55:41 +0800

    在业务逻辑上进行了一些小修改

--- mechanician_eureka/src/main/java/git/mechanician/eureka/EurekaApplication.java
+++ mechanician_eureka/src/main/java/git/mechanician/eureka/EurekaApplication.java
@@ -2,11 +2,7 @@ package git.mechanician.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.WebApplicationInitializer;
-
 /**
  * @ClassName EurekaApplication
  * @Author GitDatSanvich
