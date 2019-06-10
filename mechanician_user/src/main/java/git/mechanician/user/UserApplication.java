6d7a6b155635cbea144930a53dd88e887d6bcc80
Gitdatsanvich <tc704321764@outlook.com>
2019-06-10 19:49:26 +0800

    增加了用户服务 添加了登录/用户注册功能

---
+++ mechanician_user/src/main/java/git/mechanician/user/UserApplication.java
@@ -0,0 +1,33 @@
+package git.mechanician.user;
+
+import git.mechanician.user.utils.IdWorker;
+import git.mechanician.user.utils.SHA;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorkker() {
+        return new IdWorker(1, 1);
+    }
+
+    @Bean
+    public SHA sha() {
+        return new SHA();
+    }
+
+}
